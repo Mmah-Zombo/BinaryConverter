4c70ea54e9f097754eabb0cee7bdc4318076959f
M'mah Zombo <www.mariamzombo@gmail.com>
2024-06-05 19:20:49 +0000

    created the basic setup for the application

--- README.md
+++ README.md
@@ -2,6 +2,33 @@
 
 This is a binary converter built for a university group project.
 
+## Starting the App
+
+Open your terminal and run:
+``` bash
+npm install
+```
+
+This command installs all dependencies for the applicaiton. Next get your CSS up and running with this command:
+
+``` bash
+npm run build
+```
+
+To update the CSS in your browser, immediately when cahnges are made to the app's style, run this command:
+
+```bash
+npm run watch
+```
+
+Finally run this command to start the server:
+
+``` bash
+npm start
+```
+
+This command will only work if you have nodemon installed for the app.
+
 ## Team
 M'mah Zomb
 Josephine Magona
---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const expressSession = require('express-session');
+const fileupload = require('express-fileupload');
+const flash = require('connect-flash');
+const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/BitConvertPro');
+
+app = express();
+
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(fileupload());
+
+app.use(flash());
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(4000, () => {
+    console.log('App started at http://localhost:4000');
+});
