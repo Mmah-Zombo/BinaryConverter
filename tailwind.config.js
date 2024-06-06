e6f1fdb34c22a496765eac848e5a6236c92e8df2
Mmah-Zombo <www.mariamzombo@gmail.com>
2024-06-06 23:01:43 +0000

    designed the calculator layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,10 @@ module.exports = {
         "space-cadet": "#171738",
         "lavender-blush": "#FFEEF2",
         "pale-purple": "#FFE4F3",
+        "pale-purple-light": "#EFDEE7",
         "federal-blue": "#2E1760",
         "federal-blue-light": "#5F2FC6",
+        "button-blue": "#512BA3",
         "zaffre": "#3423A6",
         "space-light": "#41419E",
         "space-dark": "#171738"
