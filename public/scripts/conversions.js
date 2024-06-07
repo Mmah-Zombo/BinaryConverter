1db4f7febdb07a2ee4feb0e7aeb30719e21397df
Mmah-Zombo <www.mariamzombo@gmail.com>
2024-06-07 13:36:46 +0000

    fixed the 'NaN' error

--- public/scripts/conversions.js
+++ public/scripts/conversions.js
@@ -1,9 +1,9 @@
-function toDecimal(data) {
-    if (inputBox.innerText === "ENTER BINARY NUMBER") {
+function toDecimal() {
+    if (inputBox.innerText === "ENTER BINARY NUMBER" || inputBox.innerHTML === "PLEASE ENTER A BINARY NUMBER") {
         inputBox.innerHTML = "PLEASE ENTER A BINARY NUMBER";
-        setInterval(() => inputBox.innerText = "ENTER BINARY NUMBER FIRST", 3000)
+        setInterval(() => inputBox.innerText = "ENTER BINARY NUMBER", 3000)
     } else {
-        let binary_number = data || inputBox.innerText;
+        let binary_number = inputBox.innerText;
         let binary_array = [];
         for (let each in binary_number) {
             binary_array.push(parseInt(binary_number[each]));
@@ -22,12 +22,12 @@ function toDecimal(data) {
     }
 }
 
-function toHexadecimal(data) {
-    if (inputBox.innerText === "ENTER BINARY NUMBER") {
+function toHexadecimal() {
+    if (inputBox.innerText === "ENTER BINARY NUMBER" || inputBox.innerHTML === "PLEASE ENTER A BINARY NUMBER") {
         inputBox.innerHTML = "PLEASE ENTER A BINARY NUMBER";
-        setInterval(() => inputBox.innerText = "ENTER BINARY NUMBER FIRST", 3000)
+        setInterval(() => inputBox.innerText = "ENTER BINARY NUMBER", 3000)
     } else {
-        let binary = data || inputBox.innerHTML;
+        let binary = inputBox.innerHTML;
         let decimal = parseInt(binary, 2);
     
         // Convert the integer to a hexadecimal string
@@ -39,11 +39,10 @@ function toHexadecimal(data) {
     }
 }
 
-
 function toOctal() {
-    if (inputBox.innerText === "ENTER BINARY NUMBER") {
+    if (inputBox.innerText === "ENTER BINARY NUMBER" || inputBox.innerHTML === "PLEASE ENTER A BINARY NUMBER") {
         inputBox.innerHTML = "PLEASE ENTER A BINARY NUMBER";
-        setInterval(() => inputBox.innerText = "ENTER BINARY NUMBER FIRST", 3000)
+        setInterval(() => inputBox.innerText = "ENTER BINARY NUMBER", 3000);
     } else {
         let binary = inputBox.innerHTML;
         let decimal = parseInt(binary, 2);
