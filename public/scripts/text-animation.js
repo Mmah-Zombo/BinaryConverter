f88212ed50106551ef5de1a8277992fd751df85c
Mmah-Zombo <www.mariamzombo@gmail.com>
2024-06-08 17:00:23 +0000

    added text animation

---
+++ public/scripts/text-animation.js
@@ -0,0 +1,34 @@
+const words = ["WELCOME TO", "BITCONVERT PRO", "A BINARY CONVERTER"];
+let i = 0;
+let j = 0;
+let currentWord = "";
+let isDeleting = false;
+
+function type() {
+  currentWord = words[i];
+  if (isDeleting) {
+
+    document.getElementById("typewriter").textContent = currentWord.substring(0, j-1);
+    j--;
+    if (j == 0) {
+      isDeleting = false;
+      i++;
+      if (i == words.length) {
+        i = 0;
+      }
+    }
+  } else {
+    document.getElementById("typewriter").textContent = currentWord.substring(0, j+1);
+    j++;
+    if (j == currentWord.length) {
+      isDeleting = true;
+    }
+  }
+  if (j == currentWord.length) {
+    setTimeout(type, 3000);
+  } else {
+      setTimeout(type, 100);
+  }
+}
+
+type();
